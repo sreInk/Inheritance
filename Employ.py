0c67621a54619bb02155943f4204900855561b71
sreInk <srinknok@gmail.com>
2024-11-07 13:14:58 +0530

    uju

---
+++ Employ.py
@@ -0,0 +1,18 @@
+class Person(object):
+    def __init__(self,name,Idnumber):
+        self.name = name
+        self.Idnumber = Idnumber
+    def display(self):
+         print(self.name)
+         print(self.Idnumber)
+
+class Employ(Person):
+    def __init__(self, name, Idnumber,salery,Post):
+                 self.salery = salery
+                 self.Post = Post
+                 Person.__init__(self,name,Idnumber)
+
+a = Employ("Rahul Pailikar",124875445,-2000,"cleark")
+a.display
+       
+        
