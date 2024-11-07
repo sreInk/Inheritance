4f7d0b44017465ae993be402922bb1844420f530
sreInk <srinknok@gmail.com>
2024-11-07 13:01:56 +0530

    first commit

---
+++ Parnet_Child.py
@@ -0,0 +1,11 @@
+class fhone:
+    def __init__(self,name,age,speed):
+        self.name = name
+        self.speed = speed
+        self.age = age
+#No CopyRight
+class ifhone(fhone):
+    pass
+Nokia = ifhone("Iphone S 105",12,"More than Squires Killed By FBI")
+print("The Name of Phone is ",Nokia.name,"The age is ",Nokia.age,"The speed is ",Nokia.speed)
+      
